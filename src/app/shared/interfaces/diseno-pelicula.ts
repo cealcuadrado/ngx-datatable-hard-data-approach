8d91ed17dee8defefafa563c02178cae946f953b
CeAlCuadrado <cealcuadrado@outlook.com>
2021-07-22 10:45:25 -0400

    Agregada interfaz y registros en duro para usar

---
+++ src/app/shared/interfaces/diseno-pelicula.ts
@@ -0,0 +1,8 @@
+export class DisenoPelicula {
+  public titulo: string;
+  public annio: string;
+  public director: string;
+  public escritor: string;
+  public genero: string;
+  public duracion: string;
+}
